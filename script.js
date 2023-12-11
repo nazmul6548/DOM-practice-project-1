ec914fb2da66a8304e7ffe62cb3bd7b5adb3af99
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2023-12-11 18:21:23 +0600

    2nd work

--- script.js
+++ script.js
@@ -6,13 +6,29 @@ ul.style.justifyContent="space-around";
 ul.style.fontSize='20px'
 ul.style.fontFamily='Helvetica';
 ul.style.fontStyle='bold';
-ul.style.backgroundColor="aqua";
+ul.style.backgroundColor="red";
 ul.style.padding='10px'
 
 const x = document.getElementById('main-id');
 x.style.width='90%';
 x.style.margin='0 auto'
 
-const menu =document.getElementsByClassName("li");
-menu.style.liststyle="none";
-const text = document.getElementsByName
+// const menu =document.getElementsByClassName("li");
+// menu.style.liststyle="none";
+
+const p = document.getElementById('section-id');
+p.style.display='flex'
+p.style.justifyContent='space-round';
+p.style.textAlign="center"
+
+const d = document.getElementsByClassName('details');
+duu.style.fontSize='50px';
+// d.style.width="50%"
+const dd= document.getElementsByClassName('detailss');
+dd.style.fontSize="50px";
+dd.style.backgroundColor='red';
+
+
+const btn1 = document.getElementsById('btn');
+// btn1.style.backgroundColor='red';
+console.log(btn1);
