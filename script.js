9312388fe28487590b610c679401ccab62ad1557
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2023-12-11 16:42:40 +0600

    1st work

---
+++ script.js
@@ -0,0 +1,18 @@
+const ul = document.getElementById('ul-id');
+ul.style.display="flex"
+// ul.style.padding='50px'
+ul.style.listStyleType='none'
+ul.style.justifyContent="space-around";
+ul.style.fontSize='20px'
+ul.style.fontFamily='Helvetica';
+ul.style.fontStyle='bold';
+ul.style.backgroundColor="aqua";
+ul.style.padding='10px'
+
+const x = document.getElementById('main-id');
+x.style.width='90%';
+x.style.margin='0 auto'
+
+const menu =document.getElementsByClassName("li");
+menu.style.liststyle="none";
+const text = document.getElementsByName
