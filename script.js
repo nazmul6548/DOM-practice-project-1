5dce6a0e482eedd5dd1b9105932d3d6da81b8eec
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2023-12-11 19:53:12 +0600

    3rd work

--- script.js
+++ script.js
@@ -11,24 +11,32 @@ ul.style.padding='10px'
 
 const x = document.getElementById('main-id');
 x.style.width='90%';
-x.style.margin='0 auto'
-
-// const menu =document.getElementsByClassName("li");
-// menu.style.liststyle="none";
-
-const p = document.getElementById('section-id');
-p.style.display='flex'
-p.style.justifyContent='space-round';
-p.style.textAlign="center"
-
-const d = document.getElementsByClassName('details');
-duu.style.fontSize='50px';
-// d.style.width="50%"
-const dd= document.getElementsByClassName('detailss');
-dd.style.fontSize="50px";
-dd.style.backgroundColor='red';
+x.style.margin='0 auto';
 
 
-const btn1 = document.getElementsById('btn');
-// btn1.style.backgroundColor='red';
-console.log(btn1);
+const btn1 = document.getElementById('pera');
+btn1.style.backgroundColor='orange';
+btn1.style.border='none';
+btn1.style.padding='15px 25px';
+btn1.style.borderRadius='15px';
+btn1.style.color='white';
+btn1.style.fontSize='15px';
+
+
+const sec = document.getElementById('section-id');
+sec.style.display='flex';
+sec.style.fontFamily='arial';
+sec.style.textAlign='center';
+sec.style.justifyContent= 'center';
+
+
+
+const foot = document.getElementById('footer');
+foot.style.backgroundColor='#0000';
+foot.style.width='100%';
+foot.style.height='90px';
+foot.style.backgroundColor='black';
+foot.style.marginTop='20px';
+foot.style.textAlign='center';
+foot.style.lineHeight='80px';
+foot.style.color='yellow';
