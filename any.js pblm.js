d6e7e8932bbead243a8f657f62874b7f08531242
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2023-12-24 22:02:40 +0600

    dom

--- any.js pblm.js
+++ any.js pblm.js
@@ -2,10 +2,13 @@ const myObj = {
     Name: 'Shahin',
     fFruit: ['Mango', 'Apple', 'Banana'],
     printfFruit: function(){
-        this.fFruit.forEach(function(e){
-            console.log(`${this.Name} like ${e}`);
+        this.fFruit.forEach((e) =>
+            console.log(`${this.Name} like ${e}`));
             console.log(this.Name);
-        })
-    }
+        }
+    
 }
 myObj.printfFruit();
+
+const result = document.querySelector("#footer").innerHTML="<h1> 'hhhhh' </h1>";
+console.log(result);
--- script.js
+++ script.js
@@ -1,49 +1,50 @@
-// const ul = document.getElementById('ul-id');
-// ul.style.display="flex"
-// ul.style.padding='50px'
-// ul.style.listStyleType='none'
-// ul.style.justifyContent="space-around";
-// ul.style.fontSize='20px'
-// ul.style.fontFamily='Helvetica';
-// ul.style.fontStyle='bold';
-// ul.style.backgroundColor="red";
-// ul.style.padding='10px'
+const ul = document.getElementById('ul-id');
+ul.style.display="flex"
+ul.style.padding='50px'
+ul.style.listStyleType='none'
+ul.style.justifyContent="space-around";
+ul.style.fontSize='20px'
+ul.style.fontFamily='Helvetica';
+ul.style.fontStyle='bold';
+ul.style.backgroundColor="red";
+ul.style.padding='10px'
 
-// const x = document.getElementById('main-id');
-// x.style.width='90%';
-// x.style.margin='0 auto';
-
-
-// const btn1 = document.getElementById('pera');
-// btn1.style.backgroundColor='orange';
-// btn1.style.border='none';
-// btn1.style.padding='15px 25px';
-// btn1.style.borderRadius='15px';
-// btn1.style.color='white';
-// btn1.style.fontSize='15px';
+const x = document.getElementById('main-id');
+x.style.width='90%';
+x.style.margin='0 auto';
 
 
-// const sec = document.getElementById('section-id');
-// sec.style.display='flex';
-// sec.style.fontFamily='arial';
-// sec.style.textAlign='center';
-// sec.style.justifyContent= 'center';
-// sec.style.backgroundColor='#999999';
-// sec.style.padding='20px'
+const btn1 = document.getElementById('pera');
+btn1.style.backgroundColor='orange';
+btn1.style.border='none';
+btn1.style.padding='15px 25px';
+btn1.style.borderRadius='15px';
+btn1.style.color='white';
+btn1.style.fontSize='15px';
+
+
+const sec = document.getElementById('section-id');
+sec.style.display='flex';
+sec.style.fontFamily='arial';
+sec.style.textAlign='center';
+sec.style.justifyContent= 'center';
+sec.style.backgroundColor='#999999';
+sec.style.padding='20px'
 
 
 
-// const foot = document.getElementById('footer');
-// foot.style.backgroundColor='#0000';
-// foot.style.width='100%';
-// foot.style.height='90px';
-// foot.style.backgroundColor='black';
-// foot.style.marginTop='20px';
-// foot.style.textAlign='center';
-// foot.style.lineHeight='80px';
-// foot.style.color='yellow';
-// const det =document.getElementById('ills');
-// det.style.color='red';
+const foot = document.getElementById('footer');
+foot.style.backgroundColor='#0000';
+foot.style.width='100%';
+foot.style.height='90px';
+foot.style.backgroundColor='black';
+foot.style.marginTop='20px';
+foot.style.textAlign='center';
+foot.style.lineHeight='80px';
+foot.style.color='yellow';
+const det =document.getElementById('ills');
+det.style.color='red';
 
 
-// console.log(Window);
+console.log(Window);
+
